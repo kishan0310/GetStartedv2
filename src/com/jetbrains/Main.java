fc4efce450f4e508454a32bdbeb089309facac26
TUSHAR_GIT <er.tushar2014@gmail.com>
2020-05-23 17:11:17 +0530

    Initial commit: Add class and xml files.

---
+++ src/com/jetbrains/Main.java
@@ -0,0 +1,10 @@
+package com.jetbrains;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println(args[0]);
+
+    }
+}
